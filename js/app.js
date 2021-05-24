c94dbdbc7a2691e08ebd803a202f6fbf470245a7
gerlinp <gerlin.pierrelouis@gmail.com>
2021-05-24 03:05:58 -0400

    ready for the public

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
     target: '#navbar-example'
   })
+
+
+  
+
+ 
+
+/* 
+  https://gerlinp.github.io/web_app_dashboard_v3.2/
+  https://gerlinp.github.io/a_random_quote_generator-v1/
+  https://gerlinp.github.io/game_show_app_v1.2/
+  https://gerlinp.github.io/photo_gallery_v5/
+
+  */ 
